c0a6b5885acfa391a2a0b41a689fa3a271cfa192
ahmed <a.oosman.g@gmail.com>
2021-10-04 15:54:58 +0200

    v1.1.2

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-
 let btn = document.querySelector('#msg');
 let input = document.querySelector('#number');
 let number;
 
-console.log(number);
-// let x = number.toString();
-
-
 input.addEventListener("input", function(){
 number = input.value;
-console.log(isNaN(parseInt(number)));
-console.log(number);
-
   if (number !==""){
     btn.classList.remove("disabled");
   }
@@ -19,9 +11,7 @@ console.log(number);
     btn.classList.add("disabled");
   }
   btn.addEventListener("click",function(){
-    // console.log(number);
     let url = "https://wa.me/+20" + number;
-    console.log(url);
     window.location.href = url;
   });
 });
