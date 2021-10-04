98a6a52c7edf4251307aafd4122c50b8ec4c23da
ahmed <a.oosman.g@gmail.com>
2021-10-04 15:41:43 +0200

    v1.1.0

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 
 let btn = document.querySelector('#msg');
+let input = document.querySelector('#number');
+let number;
 
-btn.addEventListener("click",function(){
-  let number = document.querySelector('#number').value;
-  // console.log(number);
-  let url = "https://wa.me/+20" + number;
-  console.log(url);
-  window.location.href = url;
+console.log(number);
+// let x = number.toString();
+
+
+input.addEventListener("input", function(){
+number = input.value;
+console.log(isNaN(parseInt(number)));
+console.log(number);
+
+  if (number !==""){
+    btn.classList.remove("disabled");
+  }
+  else{
+    btn.classList.add("disabled");
+  }
+  btn.addEventListener("click",function(){
+    // console.log(number);
+    let url = "https://wa.me/+20" + number;
+    console.log(url);
+    window.location.href = url;
+  });
 });
